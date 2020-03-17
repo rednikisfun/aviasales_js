02d5591c9b3838b71e7f56862c66820501a092df
rednikisfun <rednik@mail.ru>
2020-03-18 03:37:13 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const formSearch = document.querySelector(".form-search"),
+    inputCitiesFrom = formSearch.querySelector(".input__cities-from"),
+    dropdownCitiesFrom = formSearch.querySelector(".dropdown__cities-from"),
+    inputCitiesTo = formSearch.querySelector(".input__cities-to"),
+    dropdownCitiesTo = formSearch.querySelector(".dropdown__cities-to");
+
+const city = ['Москва', 'Санкт-Петербуг', 'Минск', 'Караганда', 'Челябинск',
+    'Керчь', 'Волгоград', 'Самара', 'Днепропетровск', 'Ташкент', 'Екатеренбург', 'Одесса',
+    'Ухань', 'Шымкент', 'Нижний Новгород', 'Калининград', 'Самрканд', 'Париж'
+];
+
+
+const showCity = (input, list) => {
+    list.textContent = '';
+
+    if (input.value === '') return;
+    const filterCity = city.filter((item) => {
+        const fixItem = item.toLowerCase();
+        return fixItem.includes(input.value.toLowerCase());
+    });
+
+    filterCity.forEach((item) => {
+        const li = document.createElement('li');
+        li.classList.add('dropdown__city');
+        li.textContent = item;
+        list.append(li);
+    });
+
+};
+
+inputCitiesFrom.addEventListener('input', () => {
+    showCity(inputCitiesFrom, dropdownCitiesFrom)
+});
+
+inputCitiesTo.addEventListener('input', () => {
+    showCity(inputCitiesTo, dropdownCitiesTo)
+})
+
+dropdownCitiesFrom.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.tagName.toLowerCase() === 'li') {
+        inputCitiesFrom.value = target.textContent;
+        dropdownCitiesFrom.textContent = '';
+    }
+});
+
+dropdownCitiesTo.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.tagName.toLowerCase() === 'li') {
+        inputCitiesTo.value = target.textContent;
+        dropdownCitiesTo.textContent = '';
+    }
+})
